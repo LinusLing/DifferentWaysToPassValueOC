be4bc55ce7c1498e34612627a0e12954eccd86b7
Linus <360725966@qq.com>
2015-09-15 22:31:24 +0800

    new project

---
+++ DifferentWaysToPassValueOC/DifferentWaysToPassValueOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DifferentWaysToPassValueOC
+//
+//  Created by Linus on 15-9-15.
+//  Copyright (c) 2015年 Linus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
